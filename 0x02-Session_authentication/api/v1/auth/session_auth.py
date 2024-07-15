b41f451eaee709a5c1413cda8f2c35ad983acade
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-07-15 19:14:16 +0200

    add: session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,12 +9,18 @@ from models.user import User
 
 
 class SessionAuth(Auth):
-    """Session authentication class.
+    """
+    Session authentication class.
     """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a session id for the user.
+        """
+        Args:
+            user_id (str, optional): _description_. Defaults to None.
+
+        Returns:
+            str: _description_
         """
         if type(user_id) is str:
             session_id = str(uuid4())
@@ -22,8 +28,11 @@ class SessionAuth(Auth):
             return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Retrieves the user id of the user associated with
-        a given session id.
+        """
+        Args:
+            session_id (str, optional): _description_. Defaults to None.
+        Returns:
+            str: _description_
         """
         if type(session_id) is str:
             return self.user_id_by_session_id.get(session_id)
