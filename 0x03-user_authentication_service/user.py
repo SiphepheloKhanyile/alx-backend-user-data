04a4dca4a6e663c9851badd403f9a6c7d0d3869f
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-07-20 02:05:51 +0200

    add: user model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+"""
+0. User model
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, INTEGER, VARCHAR
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    User Model
+    """
+    __tablename__ = 'users'
+    id = Column(INTEGER, primary_key=True)
+    email = Column(VARCHAR(250), nullable=False)
+    hashed_password = Column(VARCHAR(250), nullable=False)
+    session_id = Column(VARCHAR(250))
+    reset_password = Column(VARCHAR(250))
